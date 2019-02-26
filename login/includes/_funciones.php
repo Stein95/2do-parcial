f2d8a284e67bb9fc2b1a25bfc5d80abbbc21a67c
EOjeda95 <ed.ojeda95@gmail.com>
2019-02-25 19:17:50 -0600

    acctualizacion de codigo seagrega modulos ourteam y works

--- login/includes/_funciones.php
+++ login/includes/_funciones.php
@@ -10,9 +10,30 @@ switch ($_POST["accion"]) {
 	case 'insertar_usuarios':
 	insertar_usuarios();
 	break;
+	case 'consultar_encabezado':
+	consultar_encabezado();
+	break;
+	case 'insertar_encabezado':
+	insertar_encabezado();
+	break;
+
+	case 'consultar_works':
+	consultar_works();
+	break;
+	case 'insertar_works':
+	insertar_works();
+	break;
+	case 'consultar_ourteam':
+	consultar_ourteam();
+	break;
+	case 'insertar_ourteam':
+	insertar_ourteam();
+	break;
+	
 	default:
 	break;
 }
+
 function consultar_usuarios(){
 	global $mysqli;
 	$consulta = "SELECT * FROM usuarios";
@@ -21,21 +42,70 @@ function consultar_usuarios(){
 	while($fila = mysqli_fetch_array($resultado)){
 		array_push($arreglo, $fila);
 	}
-	echo json_encode($arreglo); //Imprime el JSON ENCODEADO
+	echo json_encode($arreglo); 
 }
 function insertar_usuarios(){
 	global $mysqli;
-	$nombre_usr = $_POST["nombre"];
-	$correo_usr = $_POST["correo"];	
-	$password = $_POST["password"];	
-	$telefono_usr = $_POST["telefono"];	
-	$consultain = "INSERT INTO usuarios VALUES('','$nombre_usr','$correo_usr','$password', '$telefono_usr', 1)";
-	$resultadoin = mysqli_query($mysqli, $consultain);
-	$arregloin = [];
-	while($filain = mysqli_fetch_array($resultadoin)){
-		array_push($arregloin, $filain);
+	$nomU = $_POST["nombre"];
+	$correoU = $_POST["correo"];	
+	$passU = $_POST["password"];	
+	$telU = $_POST["telefono"];	
+	$consul1 = "INSERT INTO usuarios VALUES('','$nomU','$correoU','$passU', '$telU', 1)";
+	$resul1 = mysqli_query($mysqli, $consultain);
+	$arre1 = [];
+	while($fila1 = mysqli_fetch_array($resul1)){
+		array_push($arre1, $fila1);
 	}
-	echo json_encode($arregloin); //Imprime el JSON ENCODEADO
+	echo json_encode($arre1);
+}
+
+function consultar_works(){
+	global $mysqli;
+	$consul2 = "SELECT * FROM works";
+	$resul2 = mysqli_query($mysqli, $consulta);
+	$arre2 = [];
+	while($fila2 = mysqli_fetch_array($resul2)){
+		array_push($arre2, $fila2);
+	}
+	echo json_encode($arre2); 
+}
+function insertar_works(){
+	global $mysqli;
+	$imgW = $_POST["imagen"];
+	$proNameW= $_POST["proyecto"];	
+	$WebW = $_POST["website"];	
+	$consul3= "INSERT INTO works VALUES('','$imgW','$proNameW','$WebW')";
+	$resul3 = mysqli_query($mysqli, $consultain);
+	$arre3 = [];
+	while($fila3 = mysqli_fetch_array($resul3)){
+		array_push($arre3, $fila3);
+	}
+	echo json_encode($arre3);
+}
+
+
+function consultar_ourteam(){
+	global $mysqli;
+	$consul4 = "SELECT * FROM ourteam";
+	$resul4 = mysqli_query($mysqli, $consul4);
+	$arre4 = [];
+	while($fila4 = mysqli_fetch_array($resul4)){
+		array_push($arre4, $fila4);
+	}
+	echo json_encode($arre4);
+}
+function insertar_ourteam(){
+	global $mysqli;
+	$imgO = $_POST["imagen"];
+	$nomO = $_POST["nombre"];	
+	$cargO = $_POST["cargo"];	
+	$consul5 = "INSERT INTO ourteam VALUES('','$imgO','$nomO','$cargO')";
+	$resul5 = mysqli_query($mysqli, $consul5);
+	$arre5 = [];
+	while($fila5 = mysqli_fetch_array($resul5)){
+		array_push($arre5, $fila5);
+	}
+	echo json_encode($arregloin);
 }
 
 	function login(){
